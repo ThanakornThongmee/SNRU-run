306fe883bab11b6732273d66cf080cf09331b46b
kissofdeath <guillinkinpark@windowslive.com>
2016-05-16 16:06:27 +0700

    Design Sign up java

--- app/src/main/java/snru/thongmee/thanakorn/snrurun/MyAlert.java
+++ app/src/main/java/snru/thongmee/thanakorn/snrurun/MyAlert.java
@@ -2,6 +2,7 @@ package snru.thongmee.thanakorn.snrurun;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 
 /**
  * Created by Administrator on 16/5/2559.
@@ -12,6 +13,20 @@ public class MyAlert {
                          String strTitle,
                          String strMessage) {
         AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setCancelable(false);
+        builder.setIcon(R.drawable.doremon48);
+        builder.setTitle(strTitle);
+        builder.setMessage(strMessage);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+
+
+
     }
 
 }//Main Class
