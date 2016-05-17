0161fbaf68ade5493c1a9a136db4a96855951122
kissofdeath <guillinkinpark@windowslive.com>
2016-05-17 15:06:36 +0700

    database

---
+++ app/src/main/java/snru/thongmee/thanakorn/snrurun/MyManage.java
@@ -0,0 +1,20 @@
+package snru.thongmee.thanakorn.snrurun;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by Administrator on 17/5/2559.
+ */
+public class MyManage {
+
+    private MyOpenHelper myOpenHelper;
+    private SQLiteDatabase sqLiteDatabase;
+
+    public MyManage(Context context) {
+
+        myOpenHelper = new MyOpenHelper(context);
+        sqLiteDatabase = myOpenHelper.getWritableDatabase();
+
+    }// Constructor
+}//Main Class
