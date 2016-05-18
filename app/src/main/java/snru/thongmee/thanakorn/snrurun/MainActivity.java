bc0aa89ed2b07fdb10a71c9c74b684687d159b08
kissofdeath <guillinkinpark@windowslive.com>
2016-05-18 09:38:37 +0700

    clickSignIn

--- app/src/main/java/snru/thongmee/thanakorn/snrurun/MainActivity.java
+++ app/src/main/java/snru/thongmee/thanakorn/snrurun/MainActivity.java
@@ -1,14 +1,30 @@
 package snru.thongmee.thanakorn.snrurun;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
     //Explicit
     private MyManage myManage;
+    private ImageView imageView;
+    private EditText userEditText, passwordEditText;
+    private String userString, passwordString;
 
 
     @Override
@@ -16,9 +32,112 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Blind Widget
+        imageView = (ImageView) findViewById(R.id.imageView6);
+        userEditText = (EditText) findViewById(R.id.editText4);
+        passwordEditText = (EditText) findViewById(R.id.editText5);
+
+
+
         myManage = new MyManage(MainActivity.this);
 
+        //Test Add User
+        //myManage.addUser("ธนากร กิวล์", "guil", "00000", "4");
+
+        //Delete All SQLite
+        deleteAllSQLite();
+
+        MySynchronize mySynchronize = new MySynchronize();
+        mySynchronize.execute();
+
+        //Show Logo
+        Picasso.with(MainActivity.this)
+                .load("http://swiftcodingthai.com/snru/image/logo_snru.png")
+                .resize(200,250)
+                .into(imageView);
+
     }//main method
+
+    public void clickSignIn(View view) {
+
+        userString = userEditText.getText().toString().trim();//.trim คือตัดช่องว่างหน้า-หลัง
+        passwordString = passwordEditText.getText().toString().trim();
+
+        //check space
+        if (userString.equals("") || passwordString.equals(""){
+
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this, "มีช่องว่าง", "กรุณากรอกให้ครบทุกช่อง");
+
+        } else {
+
+
+
+        }
+
+    }   // clickSignIn
+
+
+    //Creat Inner Class
+
+    public class MySynchronize extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... params) {
+
+            try {
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url("swiftcodingthai.com/snru/get_user.php").build();
+                Response response = okHttpClient.newCall(request).execute();
+
+                return response.body().string();
+
+            } catch (Exception e) {
+                return null;
+            }
+           // return null;
+        }  //doInBack
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Log.d("Snru", "JSON==>" + s);
+
+            try {
+
+                JSONArray jsonArray = new JSONArray(s);
+                for (int i=0;i<jsonArray.length();i++) {
+
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    String strName = jsonObject.getString(myManage.column_name);
+                    String strUser = jsonObject.getString(myManage.column_user);
+                    String strPassword = jsonObject.getString(myManage.column_password);
+                    String strAvata = jsonObject.getString(myManage.column_avata);
+
+                    myManage.addUser(strName, strUser, strPassword, strAvata);
+
+                }   //for
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }//onPost
+
+
+    }//   //MySyn Class
+
+
+
+    private void deleteAllSQLite() {
+
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+                MODE_PRIVATE, null);
+        sqLiteDatabase.delete(MyManage.user_table, null, null);
+
+
+    }
 
     public void clickSignUpMain(View view) {
         startActivity(new Intent(MainActivity.this,SignUp.class));
--- app/src/main/java/snru/thongmee/thanakorn/snrurun/MyManage.java
+++ app/src/main/java/snru/thongmee/thanakorn/snrurun/MyManage.java
@@ -1,5 +1,6 @@
 package snru.thongmee.thanakorn.snrurun;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
@@ -11,10 +12,32 @@ public class MyManage {
     private MyOpenHelper myOpenHelper;
     private SQLiteDatabase sqLiteDatabase;
 
+    public static final String user_table = "userTABLE";
+    public static final String column_id = "_id";
+    public static final String column_name = "Name";
+    public static final String column_user = "User";
+    public static final String column_password = "Password";
+    public static final String column_avata = "Avata";
+
     public MyManage(Context context) {
 
         myOpenHelper = new MyOpenHelper(context);
         sqLiteDatabase = myOpenHelper.getWritableDatabase();
 
     }// Constructor
+
+    public long addUser(String strName,
+                        String strUser,
+                        String strPassword,
+                        String strAvata) {
+
+        ContentValues contentValues = new ContentValues();//เปลี่ยนตัวอักษรให้เป้นตัวเลข
+        contentValues.put(column_name, strName);
+        contentValues.put(column_user, strUser);
+        contentValues.put(column_password, strPassword);
+        contentValues.put(column_avata, strAvata);
+
+        return sqLiteDatabase.insert(user_table, null, contentValues);
+    }
+
 }//Main Class
